853df29a42add3fd8e7d928bbdef53234e95a37a
ellysaschottler <ellysastork@gmail.com>
2023-11-17 10:58:37 -0600

    added some routes we'll need on the server.js

--- Develop/server.js
+++ Develop/server.js
@@ -4,10 +4,31 @@ const app = express();
 
 const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.send("yay");
+const notesData = require("./db/db.json")
 
+// outputs the data in a json
+app.get('/api/notes', (req, res) => {
+    res.json(notesData);
 })
+
+// app.get('/', (req, res) => {
+//   res.send("yay");
+// });
+
+//serve up the homepage on the root)
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+// serve up the notes.html page
+app.get('/paths', (req, res) => {
+    res.sendFile(__dirname + 'public/notes.html')
+})
+
+// display error page if route not found
+app.get("*", (req, res) => {
+    res.status(404).send("404 error");
+});
 
 app.listener(PORT, () => {
   console.log("Listening on PORT 3001");
