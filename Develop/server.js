81ddf0bd3b911a909c4439a7ad3770396c6d42e6
ellysaschottler <ellysastork@gmail.com>
2023-11-17 14:13:59 -0600

    added middlware to parse posts and added post route

--- Develop/server.js
+++ Develop/server.js
@@ -6,8 +6,13 @@ const PORT = 3001;
 
 const notesData = require("./db/db.json")
 
+//middleware for parsing application/json
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
 // outputs the data in a json object
-app.get('/api/notes', (req, res) => {
+app.get("/api/notes", (req, res) => {
     res.json(notesData);
 })
 
@@ -20,10 +25,19 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-//serve up the notes page on the root)
+//serve up the notes page on /notes)
 app.get("/notes", (req, res) => {
     res.sendFile(__dirname + "/public/notes.html");
 });
+
+
+// post additional notes to the api
+app.post("/api/newnotes", (req, res) => {
+    notesData.push(req.body);
+    // console.log(req.body);
+    // res.json(`${req.body}`);
+});
+
 
 // display error page if route not found
 app.get("*", (req, res) => {
@@ -34,4 +48,3 @@ app.listen(PORT, () => {
   console.log("Listening on PORT 3001");
 });
 
-app.use(express.static("public"));
